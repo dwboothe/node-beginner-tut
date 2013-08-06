01e7e1567e733026df6091a182e53198bb7cdd6f
Dwayne Boothe <dwboothe@gmail.com>
2013-08-06 06:24:32 -0500

    Updated server.js to listen for process.en.PORt or 8080.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ function start(route, handle) {
     
         route(handle, pathname, response, request);    
     }
-    http.createServer(onRequest).listen(8080);
+    http.createServer(onRequest).listen(process.env.PORT || 8080);
     console.log("Server has started.");
 }
 
